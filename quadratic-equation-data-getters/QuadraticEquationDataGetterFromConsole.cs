5c32454724931d06c65497a38481f3dec3a444d8
Ziya <ziyakerimli04@gmail.com>
2023-02-26 14:06:33 +0400

    make get-data from console logic

--- quadratic-equation-data-getters/QuadraticEquationDataGetterFromConsole.cs
+++ quadratic-equation-data-getters/QuadraticEquationDataGetterFromConsole.cs
@@ -1,9 +1,19 @@
+using System.Globalization;
+
 namespace Lab1.quadratic_equation_data_getters;
 
 public class QuadraticEquationDataGetterFromConsole: IQuadraticEquationDataGetter
 {
     public QuadraticEquation getData()
     {
-        return new QuadraticEquation(1,1,1);
+        float a, b, c;
+        Console.Write("A: "); 
+        a = float.Parse(Console.ReadLine());
+        Console.Write("B: ");
+        b = float.Parse(Console.ReadLine());
+        Console.Write("C: ");
+        c = float.Parse(Console.ReadLine());        
+        
+        return new QuadraticEquation(a,b,c);
     }
 }
